65e57781a0fe2bc7e8c982122b6b8eae2532cf08
Ajai <forgithub40@gmail.com>
2026-03-29 07:16:28 +0530

    Update stop.c

--- ntwrklab_s6/sliding_protocols/stop.c
+++ ntwrklab_s6/sliding_protocols/stop.c
@@ -4,31 +4,23 @@ int main()
 {
     int frames[50], f, i;
     int retry;
-
     printf("Enter number of frames: ");
     scanf("%d", &f);
-
     printf("Enter %d frames (-1 for error):\n", f);
     for (i = 0; i < f; i++)
         scanf("%d", &frames[i]);
-
     printf("\n--- STOP AND WAIT PROTOCOL ---\n\n");
-
     for (i = 0; i < f; i++)
     {
         retry = 0;
-
         while (1)
         {
             printf("Sending frame %d...\n", i + 1);
-
             if (frames[i] == -1)
             {
                 retry++;
                 printf("Error: Frame %d lost! Retry %d\n\n",
                        i + 1, retry);
-
-   
                 frames[i] = (i + 1) * 10;
             }
             else
@@ -39,7 +31,6 @@ int main()
             }
         }
     }
-
     return 0;
 }
 
